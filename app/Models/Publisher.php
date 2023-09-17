2dafb973bf107c4709de1a8ff336c1c54ec5cb2b
kujawapatryk <homik363@wp.pl>
2023-09-17 15:03:38 +0200

    Added the 'publisher' model and relationships with 'game'

---
+++ app/Models/Publisher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Publisher extends Model
+{
+    public function games():BelongsToMany{
+        return$this->belongsToMany('App\Model\Game', 'gamePublishers');
+    }
+}
