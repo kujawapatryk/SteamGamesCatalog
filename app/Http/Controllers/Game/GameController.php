da9552893998b5f3ebbe6ce44abdc81b7686a134
kujawapatryk <homik363@wp.pl>
2023-09-20 01:08:37 +0200

    GameController index

---
+++ app/Http/Controllers/Game/GameController.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Game;
+
+use App\Http\Controllers\Controller;
+use App\Repository\Interface\GameRepositoryInterface;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class GameController extends Controller{
+    private GameRepositoryInterface $gameRepository;
+
+    public function __construct(GameRepositoryInterface $repository)
+    {
+        $this->gameRepository = $repository;
+    }
+
+
+    public function index(Request $request): View{
+        $phrase = $request->get('phrase');
+        $type = $request->get('type', GameRepositoryInterface::TYPE_DEFAULT);
+        $limit = $request->get('size', GameRepositoryInterface::LIMIT);
+
+        $resultPagination = $this->gameRepository->filterBy($phrase, $type, $limit);
+        $resultPagination->appends([
+            'phrase' => $phrase,
+            'type' => $type,
+        ]);
+
+        return view('game.list',[
+            'games' => $resultPagination,
+            'phrase' => $phrase,
+            'type' => $type,
+        ]);
+    }
+}
