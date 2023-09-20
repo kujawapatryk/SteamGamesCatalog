c8304eb82015d3abedfb7e6c0f5b4562822b9ae9
kujawapatryk <homik363@wp.pl>
2023-09-21 00:39:45 +0200

    sidebar

---
+++ resources/views/shared/sidebar.blade.php
@@ -0,0 +1,35 @@
+<!-- Link do Panelu -->
+{{--<a href="{{ route('home.mainPage') }}" class="flex items-center p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">--}}
+{{--    <span class="mr-2 text-xl"><i class="fas fa-home"></i></span>--}}
+{{--    Panel--}}
+{{--</a>--}}
+
+<!-- Nagłówek "Konto" -->
+<div class="text-gray-500 uppercase tracking-wide font-bold mt-4 mb-2">Konto</div>
+
+<!-- Linki w sekcji "Konto" -->
+<nav class="space-y-2">
+    {{--    <a href="{{ route('me.profile') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Profil</a>--}}
+    {{--    <a href="{{ route('me.games.list') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Gry</a>--}}
+</nav>
+
+<!-- Nagłówek "Gry" -->
+<div class="text-gray-500 uppercase tracking-wide font-bold mt-4 mb-2">Gry</div>
+
+<!-- Linki w sekcji "Gry" -->
+<nav class="space-y-2">
+    sssssssss
+    {{--    <a href="{{ route('games.dashboard') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Dashboard</a>--}}
+    <a href="{{ route('games.list') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Katalog</a>
+</nav>
+
+@can('admin-level')
+    <!-- Nagłówek "Admin panel" -->
+    <div class="text-gray-500 uppercase tracking-wide font-bold mt-4 mb-2">Admin panel</div>
+
+    <!-- Linki w sekcji "Admin panel" -->
+    <nav class="space-y-2">
+        ssssssssssssssssssssss
+        {{--        <a href="{{ route('get.users') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Użytkownicy</a>--}}
+    </nav>
+@endcan
