2b99548c45873fc5d77df8bfc5c83f68710e3413
kujawapatryk <homik363@wp.pl>
2023-09-21 03:49:57 +0200

    Adding retrieval by metacritic_score

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -15,5 +16,11 @@ class Game extends Model
     public function publishers(): BelongsToMany
     {
         return $this->belongsToMany('App\Models\Publisher', 'gamePublishers');
+    }
+
+    public function scopeBest(Builder $query): Builder
+    {
+        return $query->where('metacritic_score', '>' , 70)
+            ->orderBy('metacritic_score', 'desc');
     }
 }
