f9ac979032d01a7edb3c688956d8fceff5fbe966
kujawapatryk <homik363@wp.pl>
2023-09-22 00:19:57 +0200

    User repository and interface.

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(
+            UserRepositoryInterface::class,
+            UserRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
