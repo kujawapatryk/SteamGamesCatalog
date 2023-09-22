34ff48cd169e37b6d22a1bbc5e79c9ef5e9ffd7a
kujawapatryk <homik363@wp.pl>
2023-09-22 21:23:19 +0200

    Adding the appearance of the user's game list.

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -10,7 +10,7 @@
     <!-- Linki w sekcji "Konto" -->
     <nav class="space-y-2">
         {{--    <a href="{{ route('me.profile') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Profil</a>--}}
-        {{--    <a href="{{ route('me.games.list') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Gry</a>--}}
+            <a href="{{ route('user.games.list') }}" class="block p-2 hover:bg-gray-200 rounded transition duration-150 ease-in-out">Gry</a>
     </nav>
 
     <!-- Nagłówek "Gry" -->
--- resources/views/user/game/list.blade.php
+++ resources/views/user/game/list.blade.php
@@ -1,74 +1,69 @@
-@extends('layout.main')
+@extends('layouts.main')
 
 @section('content')
-    <div class="card mt-3">
-        <div class="card-header"><i class="fas fa-table mr-1"></i>Moje gry</div>
-        <div class="card-body">
-
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th>Lp</th>
-                        <th>Tytuł</th>
-                        <th>Kategoria</th>
-                        <th>Ocena</th>
-                        <th>Twoja ocena</th>
-                        <th>Opcje</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($games ?? [] as $game)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $game->name }}</td>
-                            <td>{{ $game->genres->implode('name', ', ') }}</td>
-                            <td>
-                                {{ $game->score ?? 'brak' }}
-                            </td>
-                            <td>
-                                <form class="m-0" method="post" action="{{ route('user.games.rate') }}">
-                                    @csrf
-                                    <div class="form-row">
-                                        <input type="hidden" name="gameId" value="{{ $game->id }}">
-                                        <div class="col-auto">
-                                            <input
-                                                class="form-control mb-2"
-                                                placeholder="ocena"
-                                                name="rate"
-                                                type="number"
-                                                max="100"
-                                                min="1"
-                                                value="{{ $game->pivot->rate }}"
-                                            />
-                                        </div>
-                                        <div class="col-auto">
-                                            <button type="submit" class="btn btn-primary mb-2">Oceń</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </td>
-                            <td>
-                                <a href="{{ route('games.show', ['game' => $game->id]) }}">Szczegóły</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-            {{ $games->links() }}
+    <div class="bg-white mt-3 p-6 rounded shadow">
+        <div class="flex items-center mb-4">
+            <i class="fas fa-table mr-1"></i>
+            <span class="text-lg font-bold">Moje gry</span>
         </div>
 
+        @if ($errors->any())
+            <div class="bg-red-500 text-white p-4 rounded mb-4">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="overflow-x-auto">
+            <table class="min-w-full bg-white border border-gray-300">
+                <thead>
+                <tr>
+                    <th class="py-2 px-4 border-b">Lp</th>
+                    <th class="py-2 px-4 border-b">Tytuł</th>
+                    <th class="py-2 px-4 border-b">Kategoria</th>
+                    <th class="py-2 px-4 border-b">Ocena</th>
+                    <th class="py-2 px-4 border-b">Twoja ocena</th>
+                    <th class="py-2 px-4 border-b">Opcje</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($games ?? [] as $game)
+                    <tr>
+                        <td class="py-2 px-4 border-b">{{ $loop->iteration }}</td>
+                        <td class="py-2 px-4 border-b">{{ $game->name }}</td>
+                        <td class="py-2 px-4 border-b">{{ $game->genres->implode('name', ', ') }}</td>
+                        <td class="py-2 px-4 border-b">
+                            {{ $game->score ?? 'brak' }}
+                        </td>
+                        <td class="py-2 px-4 border-b">
+                            <form class="m-0 flex items-start" method="post" action="{{ route('user.games.rate') }}">
+                                @csrf
+                                <input type="hidden" name="gameId" value="{{ $game->id }}">
+                                <input
+                                    class="form-input mb-2 mr-2"
+                                    placeholder="ocena"
+                                    name="rate"
+                                    type="number"
+                                    max="100"
+                                    min="1"
+                                    value="{{ $game->pivot->rate }}"
+                                />
+                                <button type="submit" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline-grey active:bg-gray-800">Oceń</button>
+                            </form>
+                        </td>
+                        <td class="py-2 px-4 border-b">
+                            <a href="{{ route('games.show', ['game' => $game->id]) }}" class="text-blue-500 hover:underline">Szczegóły</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="mt-4">
+            {{ $games->links() }}
+        </div>
     </div>
 @endsection
-
