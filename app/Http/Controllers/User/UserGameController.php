c84f367a47c5b6ad5c43851cb94ceb4196245772
kujawapatryk <homik363@wp.pl>
2023-09-23 02:49:18 +0200

    Code refactoring

--- app/Http/Controllers/User/UserGameController.php
+++ app/Http/Controllers/User/UserGameController.php
@@ -5,25 +5,29 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Game\AddGameToUserList;
+use App\Http\Requests\Game\RateGame;
 use App\Http\Requests\Game\RemoveGameFromUserList;
 use App\Repository\Interface\GameRepositoryInterface;
+use App\Repository\Interface\UserGameRepositoryInterface;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 
+
 class UserGameController extends Controller{
 
     private GameRepositoryInterface $gameRepository;
-    public function __construct(GameRepositoryInterface $repository){
+    private UserGameRepositoryInterface $userGamesRepository;
+    public function __construct(GameRepositoryInterface $repository, UserGameRepositoryInterface $userGamesRepository){
         $this->gameRepository = $repository;
+        $this->userGamesRepository = $userGamesRepository;
     }
 
     public function add(AddGameToUserList $request): RedirectResponse{
         $gameId = (int) $request->input('gameId');
         $game = $this->gameRepository->get($gameId);
-        $user = Auth::user();
-        $user->addGame($game);
+        $this->userGamesRepository->addGame($game);
 
         return redirect()
             ->route('games.show', ['game' => $gameId])
@@ -33,9 +37,7 @@ class UserGameController extends Controller{
     public function remove(RemoveGameFromUserList $request): RedirectResponse{
         $gameId = (int) $request->input('gameId');
         $game = $this->gameRepository->get($gameId);
-
-        $user = Auth::user();
-        $user->removeGame($game);
+        $this->userGamesRepository->removeGame($game);
 
         return redirect()
             ->route('games.show', ['game' => $gameId])
@@ -50,4 +52,17 @@ class UserGameController extends Controller{
             'games' => $user->games()->paginate()
         ]);
     }
+
+    public function rate(RateGame $request): RedirectResponse{
+        $gameId = (int) $request->input('gameId');
+        $rate = $request->input('rate') ? (int) $request->input('rate') : null;
+        $game = $this->gameRepository->get($gameId);
+        $this->userGamesRepository->rateGame($game, $rate);
+
+
+        return redirect()
+            ->route('user.games.list')
+            ->with('success', 'Ocena została zapisana');
+    }
+
 }
