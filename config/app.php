e70dae81a563b3328de568c9706793b2fa0c8924
kujawapatryk <homik363@wp.pl>
2023-09-23 02:48:33 +0200

    Adding providers.

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\GameServiceProvider::class,
+        \App\Providers\UserServiceProvider::class,
+        \App\Providers\UserGameServiceProvider::class,
     ])->toArray(),
 
     /*
