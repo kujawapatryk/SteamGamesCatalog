77595afe00c79613cc878a623dd0a0140e2db360
kujawapatryk <homik363@wp.pl>
2023-09-25 15:57:31 +0200

    Code refactoring.

--- resources/views/profile/partials/update-avatar-form.blade.php
+++ resources/views/profile/partials/update-avatar-form.blade.php
@@ -40,7 +40,7 @@
 
             <div>
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Zapisz dane</button>
-                {{-- <a href="{{ route('user.profile') }}" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">Anuluj</a> --}}
+
             </div>
         </form>
     </div>
