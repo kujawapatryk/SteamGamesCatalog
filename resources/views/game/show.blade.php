1084a756f47ea2fb5980a1ccad38e57faff73b59
kujawapatryk <homik363@wp.pl>
2023-09-25 20:47:51 +0200

    Adding text boldness to headers.

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="card mt-3">
     @if(!empty($game))
-        <h5 class="card-header">
-            {{ $game->name }}
+        <h1 class="text-3xl font-bold inline">
+            {{ $game->name }}</h1>
             @auth
                 @if ($userHasGame)
                     <form class="float-right m-0" method="post" action="{{ route('user.games.remove') }}">
                         @method('delete')
                         @csrf
                         <div class="flex items-center space-x-4">
-                            <input type="hidden" name="gameId" value="{{ $game->id }}">
+                            <h2> <input type="hidden" name="gameId" value="{{ $game->id }}"> </h2>
                             <x-btn label="Usuń z listy"></x-btn>
                   </div>
                     </form>
@@ -26,27 +26,27 @@
                     </form>
                 @endif
             @endauth
-        </h5>
+
         <div class="card-body">
 
             <ul>
-                <li>Id: {{ $game->id }}</li>
-                <li>Nazwa: {{ $game->name }}</li>
-                <li>Wydawca: {{ $game->publishers->implode('name', ', ') }}</li>
-                <li>Gatunek:{{ $game->genres->implode('name', ', ') }}</li>
+                <li><span class="text-lg font-bold">Id:</span> {{ $game->id }}</li>
+                <li><span class="text-lg font-bold">Nazwa:</span> {{ $game->name }}</li>
+                <li><span class="text-lg font-bold">Wydawca:</span> {{ $game->publishers->implode('name', ', ') }}</li>
+                <li><span class="text-lg font-bold">Gatunek:</span>{{ $game->genres->implode('name', ', ') }}</li>
             </ul>
             <div class="my-4">
-                <h4>Krótki opis</h4>
+                <h4 class="text-xl font-bold">Krótki opis</h4>
                 <div class="mx-2">{!! $game->short_description !!}</div>
             </div>
 
             <div class="my-4">
-                <h4>Opis</h4>
+                <h4 class="text-xl font-bold">Opis</h4>
                 <div class="mx-2">{!! $game->description !!}</div>
             </div>
 
             <div class="my-4">
-                <h4>About</h4>
+                <h4 class="text-xl font-bold">About</h4>
                 <div class="mx-2">{!! $game->about !!}</div>
             </div>
 
@@ -54,7 +54,7 @@
 
         </div>
     @else
-        <h5 class="card-header">Brak danych do wyświetlenia</h5>
+        <h5 class="card-header text-xxl font-bold">Brak danych do wyświetlenia</h5>
     @endif
 </div>
 @endsection
