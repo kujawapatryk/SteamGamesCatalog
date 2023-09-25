575afce3bb7cf8dc751ea5f0cd69ef8376ba6f9d
kujawapatryk <homik363@wp.pl>
2023-09-25 14:53:29 +0200

    Displaying the avatar in the navigation next to the username.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,7 +25,11 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
+                            @if(Auth::user()->avatar)
+                            <img src="{{ Storage::url(Auth::user()->avatar) }}" class="rounded-full max-w-11 max-h-11 pl-2" alt="Avatar użytkownika">
+                            @else
+                                <img src="/images/avatar.png" class="rounded-full max-w-11 max-h-11 pl-2" alt="Avatar użytkownika">
+                            @endif
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
