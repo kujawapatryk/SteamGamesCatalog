f01a35bda232041589dd4831d0818f850b379a91
kujawapatryk <homik363@wp.pl>
2023-10-12 02:19:11 +0200

    Adding an import of the repository created in QueryBuilder.

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -1,10 +1,14 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Repository\Interface\UserRepositoryInterface;
-use App\Repository\UserRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Repository\Interface\UserRepositoryInterface;
+
+use App\Repository\Eloquent\UserRepository;
+//use App\Repository\QueryBuilder\UserRepository;
+
 
 class UserServiceProvider extends ServiceProvider
 {
