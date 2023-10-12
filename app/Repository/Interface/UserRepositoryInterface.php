ca234bc4f32e070161ed1dc05b3e50de67c5e689
kujawapatryk <homik363@wp.pl>
2023-10-12 02:19:21 +0200

    Moving to the 'eloquent' subfolder.

--- app/Repository/Interface/UserRepositoryInterface.php
+++ app/Repository/Interface/UserRepositoryInterface.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace App\Repository\Interface;
 
-use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
+use App\Models\User;  //ELOQUENT
+use Illuminate\Database\Eloquent\Collection;   //ELOQUENT
 
+//use Illuminate\Database\Query\Builder;  //QUERY BUILDER
+//use Illuminate\Support\Collection;     //QUERY BUILDER
 interface UserRepositoryInterface
 {
+//    ELOQUENT
+    public function all(): Collection;
+    public function get(int $id): User;
 
-    public function all():Collection;
-    public function get(int $id):User;
+    //QUERY BUILDER
+//    public function all(): Collection;
+//    public function get(int $id): Builder;
 }
